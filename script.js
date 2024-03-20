d7f2c0b53c4fa9d32a45d8a919a722eee7948637
B.HaEun <72196288+Bananang@users.noreply.github.com>
2024-03-21 00:52:54 +0900

    Add files via upload
    4th commit

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
-const modal = document.querySelector("dialog");
-const ticketPrice = document.querySelector(".ticket_fee");
-const inputAge = document.querySelector(".input_age");
 document.querySelector(".submit").addEventListener("click", () => {
   const ageInput = document.querySelector(".ageValue");
   let age = ageInput.value;
-  let price = 0;
-  let person = null;
+  let person = "";
 
   if (!age || isNaN(age)) {
     alert("입력형식에 맞춰 다시 입력해주세요.");
   } else {
     if (age < 3) {
-      // alert("0~36개월 미만의 유아는 파크 입장 무료 입니다.");
-      price = "무료";
-      person = "유아";
+      alert("0~36개월 미만의 유아는 파크 입장 무료 입니다.");
       ageInput.value = null;
       ageInput.focus();
-      inputAge.textContent = `입력한 나이: ${age}세`;
-      ticketPrice.textContent = `${person}: ${price}`;
     } else {
       if (3 <= age && age <= 13) {
-        price = "47,000원";
         person = "어린이";
       } else if (age <= 19) {
-        price = "54,000원";
         person = "청소년";
       } else if (age < 65) {
-        price = "62,000원";
         person = "어른";
       } else {
-        price = "47,000원";
-        person = "65세 이상";
+        person = "65세 이상이므로 어린이 할인";
       }
+      alert(`입력하신 나이는 ${age}세로 ${person} 요금이 적용됩니다.`);
       ageInput.value = null;
       ageInput.focus();
-      inputAge.textContent = `입력한 나이: ${age}세`;
-      ticketPrice.textContent = `${person}: ${price}`;
     }
-    modal.showModal();
   }
-});
-
-document.querySelector(".closeBtn").addEventListener("click", () => {
-  modal.close();
-});
-
-modal.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) modal.close();
 });
